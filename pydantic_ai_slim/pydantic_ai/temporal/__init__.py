694fa6b2f8705ab0988cdae4197cff579ef1ee9b
Douwe Maan <hi@douwe.me>
2025-07-25 20:56:49 +0000

    Use Temporal plugins

--- pydantic_ai_slim/pydantic_ai/temporal/__init__.py
+++ pydantic_ai_slim/pydantic_ai/temporal/__init__.py
@@ -1,9 +1,18 @@
 from __future__ import annotations
 
-import contextlib
+from collections.abc import Sequence
+from dataclasses import replace
 from typing import Any, Callable
 
-from temporalio import workflow
+import logfire  # TODO: Not always available
+from opentelemetry import trace  # TODO: Not always available
+from temporalio.client import ClientConfig, Plugin as ClientPlugin
+from temporalio.contrib.opentelemetry import TracingInterceptor
+from temporalio.contrib.pydantic import pydantic_data_converter
+from temporalio.runtime import OpenTelemetryConfig, Runtime, TelemetryConfig
+from temporalio.service import ConnectConfig, ServiceClient
+from temporalio.worker import Plugin as WorkerPlugin, WorkerConfig
+from temporalio.worker.workflow_sandbox import SandboxedWorkflowRunner
 
 from pydantic_ai.agent import Agent
 from pydantic_ai.toolsets.abstract import AbstractToolset
@@ -15,17 +24,66 @@ from ._settings import TemporalSettings
 from ._toolset import temporalize_toolset
 
 __all__ = [
-    'initialize_temporal',
     'TemporalSettings',
     'TemporalRunContext',
+    'PydanticAIPlugin',
+    'LogfirePlugin',
+    'AgentPlugin',
 ]
 
 
-def initialize_temporal():
-    """Initialize Temporal."""
-    with workflow.unsafe.imports_passed_through():
-        with contextlib.suppress(ModuleNotFoundError):
-            import pandas  # pyright: ignore[reportUnusedImport]  # noqa: F401
+class PydanticAIPlugin(ClientPlugin, WorkerPlugin):
+    """Temporal client and worker plugin for Pydantic AI."""
+
+    def configure_client(self, config: ClientConfig) -> ClientConfig:
+        config['data_converter'] = pydantic_data_converter
+        return super().configure_client(config)
+
+    def configure_worker(self, config: WorkerConfig) -> WorkerConfig:
+        runner = config.get('workflow_runner')  # pyright: ignore[reportUnknownMemberType]
+        if isinstance(runner, SandboxedWorkflowRunner):
+            config['workflow_runner'] = replace(
+                runner,
+                restrictions=runner.restrictions.with_passthrough_modules(
+                    'pydantic_ai',
+                    'logfire',
+                    # Imported inside `logfire._internal.json_encoder` when running `logfire.info` inside an activity with attributes to serialize
+                    'attrs',
+                    # Imported inside `logfire._internal.json_schema` when running `logfire.info` inside an activity with attributes to serialize
+                    'numpy',
+                    'pandas',
+                ),
+            )
+        return super().configure_worker(config)
+
+
+class LogfirePlugin(ClientPlugin):
+    """Temporal client plugin for Logfire."""
+
+    def configure_client(self, config: ClientConfig) -> ClientConfig:
+        config['interceptors'] = [TracingInterceptor(trace.get_tracer('temporal'))]
+        return super().configure_client(config)
+
+    async def connect_service_client(self, config: ConnectConfig) -> ServiceClient:
+        # TODO: Do we need this here?
+        logfire.configure(console=False)
+        logfire.instrument_pydantic_ai()
+        logfire.instrument_httpx(capture_all=True)
+
+        config.runtime = Runtime(telemetry=TelemetryConfig(metrics=OpenTelemetryConfig(url='http://localhost:4318')))
+        return await super().connect_service_client(config)
+
+
+class AgentPlugin(WorkerPlugin):
+    """Temporal worker plugin for a specific Pydantic AI agent."""
+
+    def __init__(self, agent: Agent[Any, Any], settings: TemporalSettings | None = None):
+        self.activities = temporalize_agent(agent, settings)
+
+    def configure_worker(self, config: WorkerConfig) -> WorkerConfig:
+        activities: Sequence[Callable[..., Any]] = config.get('activities', [])  # pyright: ignore[reportUnknownMemberType]
+        config['activities'] = [*activities, *self.activities]
+        return super().configure_worker(config)
 
 
 def temporalize_agent(
@@ -47,7 +105,8 @@ def temporalize_agent(
 
     settings = settings or TemporalSettings()
 
-    # TODO: Doesn't consider model/toolsets passed at iter time.
+    # TODO: Doesn't consider model/toolsets passed at iter time, raise an error if that happens.
+    # Similarly, passing event_stream_handler at iter time should raise an error.
 
     activities: list[Callable[..., Any]] = []
     if isinstance(agent.model, Model):
--- temporal.py
+++ temporal.py
@@ -1,36 +1,24 @@
-# /// script
-# dependencies = [
-#   "temporalio",
-#   "logfire",
-# ]
-# ///
 import asyncio
 import random
 from collections.abc import AsyncIterable
 from datetime import timedelta
 
 import logfire
-from opentelemetry import trace
 from temporalio import workflow
 from temporalio.client import Client
-from temporalio.contrib.opentelemetry import TracingInterceptor
-from temporalio.contrib.pydantic import pydantic_data_converter
-from temporalio.runtime import OpenTelemetryConfig, Runtime, TelemetryConfig
 from temporalio.worker import Worker
-from temporalio.worker.workflow_sandbox import SandboxedWorkflowRunner, SandboxRestrictions
 from typing_extensions import TypedDict
 
 from pydantic_ai import Agent, RunContext
 from pydantic_ai.mcp import MCPServerStdio
 from pydantic_ai.messages import AgentStreamEvent, HandleResponseEvent
 from pydantic_ai.temporal import (
+    AgentPlugin,
+    LogfirePlugin,
+    PydanticAIPlugin,
     TemporalSettings,
-    initialize_temporal,
-    temporalize_agent,
 )
 from pydantic_ai.toolsets import FunctionToolset
-
-initialize_temporal()
 
 
 class Deps(TypedDict):
@@ -59,7 +47,7 @@ async def event_stream_handler(
         logfire.info(f'{event=}')
 
 
-my_agent = Agent(
+agent = Agent(
     'openai:gpt-4o',
     toolsets=[toolset, mcp_server],
     event_stream_handler=event_stream_handler,
@@ -74,7 +62,6 @@ temporal_settings = TemporalSettings(
         },
     },
 )
-activities = temporalize_agent(my_agent, temporal_settings)
 
 
 TASK_QUEUE = 'pydantic-ai-agent-task-queue'
@@ -84,44 +71,26 @@ TASK_QUEUE = 'pydantic-ai-agent-task-queue'
 class MyAgentWorkflow:
     @workflow.run
     async def run(self, prompt: str, deps: Deps) -> str:
-        result = await my_agent.run(prompt, deps=deps)
+        result = await agent.run(prompt, deps=deps)
         return result.output
 
 
+# TODO: For some reason, when I put this (specifically the temporalize_agent call) inside `async def main()`,
+# we get tons of errors.
+plugin = AgentPlugin(agent, temporal_settings)
+
+
 async def main():
-    def init_runtime_with_telemetry() -> Runtime:
-        logfire.configure(console=False)
-        logfire.instrument_pydantic_ai()
-        logfire.instrument_httpx(capture_all=True)
-
-        # Setup SDK metrics to OTel endpoint
-        return Runtime(telemetry=TelemetryConfig(metrics=OpenTelemetryConfig(url='http://localhost:4318')))
-
     client = await Client.connect(
         'localhost:7233',
-        interceptors=[  # TODO: Use ClientPlugin.configure_client for this
-            TracingInterceptor(trace.get_tracer('temporal'))
-        ],
-        data_converter=pydantic_data_converter,  # TODO: Use ClientPlugin.configure_client for this
-        runtime=init_runtime_with_telemetry(),  # TODO: Use ClientPlugin.connect_service_client for this
+        plugins=[PydanticAIPlugin(), LogfirePlugin()],
     )
 
     async with Worker(
         client,
         task_queue=TASK_QUEUE,
         workflows=[MyAgentWorkflow],
-        activities=activities,
-        workflow_runner=SandboxedWorkflowRunner(  # TODO: Use WorkerPlugin.configure_worker for this, see https://github.com/temporalio/sdk-python/blob/da6616a93e9ee5170842bb5a056e2383e18d07c6/tests/test_plugins.py#L71
-            restrictions=SandboxRestrictions.default.with_passthrough_modules(
-                'pydantic_ai',
-                'logfire',
-                # Imported inside `logfire._internal.json_encoder` when running `logfire.info` inside an activity with attributes to serialize
-                'attrs',
-                # Imported inside `logfire._internal.json_schema` when running `logfire.info` inside an activity with attributes to serialize
-                'numpy',
-                'pandas',
-            ),
-        ),
+        plugins=[plugin],
     ):
         output = await client.execute_workflow(  # pyright: ignore[reportUnknownMemberType]
             MyAgentWorkflow.run,
